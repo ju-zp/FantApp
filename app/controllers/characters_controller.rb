deee4b009999c43db76ab4960baa4c6cb18de08a
richardmaccaw <ricci6@gmail.com>
2018-11-12 16:28:15 +0000

    added show and index to character controller

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,4 +1,14 @@
 class CharactersController < ApplicationController
+
+    def index
+        @characters = Character.all
+    end
+
     def create 
     end
+
+    def show
+        @character = Character.find(params[:id])
+    end
+
 end
