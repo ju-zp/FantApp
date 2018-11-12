5166cf6e19822d9fa5da5810a9b367da239aeab0
ju-zp <jhuntington@tutanota.com>
2018-11-12 16:01:44 +0000

    added create form for character, now can create a character

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,4 +1,23 @@
 class CharactersController < ApplicationController
+
+    def new
+        @character = Character.new
+    end
+
     def create 
+        @character = Character.new(character_params)
+        @character.user_id = session[:user_id]
+        if @character.save
+            redirect_to @character
+        else
+            byebug 
+            render 'new'
+        end
+    end
+
+    private 
+
+    def character_params
+        params.require(:character).permit(:name, :gender, :race_id)
     end
 end
