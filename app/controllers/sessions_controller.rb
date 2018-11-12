efffa73af55a1a6443b97a2e3d7a9fe5637ea566
ju-zp <jhuntington@tutanota.com>
2018-11-12 14:19:51 +0000

    added login page and functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        @user = User.find_by(username: params[:username].downcase)
+        return head(:forbidden) unless @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to @user
+    end
+
+    def destroy
+    end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@
 User.destroy_all
 Race.destroy_all
 
-User.create(username:"Ricky")
-User.create(username:"Joe")
+User.create(username:"ricky", password:"admin")
+User.create(username:"joe", password:"admin")
 
 Race.create(name:"Orc", bio:"Orcs are carnivorous humanoids, standing approximately 5'11' to 6'2', weighing from 180 to 280 lbs. They are easily noticeable due to their green to gray skin, lupine ears, lower canines resembling boar tusks, and their muscular builds. Orcs stand in a bent over shape making them appear as ape-like humans.")
 Race.create(name:"Grey Elf", bio:"Also known as Mountain Elves, these elves are the most noble of elves, yet also the most arrogant. They are of higher intellectual capabilities than other elves, but, despite the fact they are taller than high elves, they are physically weaker. They live in isolated mountain strongholds, and rarely allow access to outsiders. They have silver hair and amber eyes, or gold hair and violet eyes, and wear clothes of white, silver, yellow and gold, and usually wear regally colored cloaks.")
