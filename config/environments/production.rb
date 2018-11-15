c79e550acfdd1daebed1699fc1ef43f28f40652b
ju-zp <jhuntington@tutanota.com>
2018-11-15 10:10:54 +0000

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.web_socket_server_url = "wss://nameless-meadow-33075.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://nameless-meadow-33075.herokuapp.com', 'http://nameless-meadow-33075.herokuapp.com']
   # Code is not reloaded between requests.
   config.cache_classes = true
 
